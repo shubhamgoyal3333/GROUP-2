c733fc4f89e9ccb5926008ec7610ec279569de53
shubhamgoyal3333 <cse140001031@iiti.ac.in>
2016-04-07 19:47:00 +0530

    Second Iteration of Code

---
+++ registration/app/src/main/java/com/example/shubhamgoyal/login_signup/ParseApplication.java
@@ -0,0 +1,31 @@
+package com.example.shubhamgoyal.login_signup;
+
+/**
+ * Created by SHUBHAM GOYAL on 01-3-2016.
+ */
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Add your initialization code here
+        Parse.initialize(this, "0QJ5HPOgxOvJoxOqpADq5oakXLCJi4z8SaXBTBNA", "FS0AnBkNWIO0kxdnvVWAKkU2D7fuzV9ZBitcjE5q");
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        // If you would like all objects to be private by default, remove this
+        // line.
+        defaultACL.setPublicReadAccess(true);
+
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+
+}
